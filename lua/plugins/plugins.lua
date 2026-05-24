15f43a448b7f7ef0dd5ae7f95882dd1123bc76ed
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2026-05-24 18:37:43 -0400

    Add tab support with barbar.nvim

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -114,6 +114,18 @@ return {
 			},
 		},
 	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {},
+	},
 	{
 		"saghen/blink.cmp",
 		dependencies = { "rafamadriz/friendly-snippets" },
